e9d7e8814fd4e118b30f317f2daf4140e371c08b
ccabello223 <carloscabello0601@gmail.com>
2025-08-05 14:57:47 +0000

    DAdd: Invoice List and clear filter

--- webapp/controller/MainView.controller.ts
+++ webapp/controller/MainView.controller.ts
@@ -1,12 +1,51 @@
 import Controller from "sap/ui/core/mvc/Controller";
+import Filter from "sap/ui/model/Filter";
+import FilterOperator from "sap/ui/model/FilterOperator";
+import JSONModel from "sap/ui/model/json/JSONModel";
+import ListBinding from "sap/ui/model/ListBinding"
 
 /**
  * @namespace cacsfiori.invoices.controller
+ * @namespace cacsfiori.invoices.Filter
+ * @namespace cacsfiori.invoices.FilterOperator
  */
-export default class MainView extends Controller {
+export default class MainView extends Controller{
 
     /*eslint-disable @typescript-eslint/no-empty-function*/
     public onInit(): void {
-
+        const oDataJSON = new JSONModel();
+        const oView = this.getView();
+        oDataJSON.loadData("./model/SelectionScreenMenu.json");
+        oView?.setModel(oDataJSON, "selectionScreen");
+        console.log(oDataJSON.getProperty("/Countries")); 
     }
+
+    public onFilter(oEvent:any): void{
+        const ShipName = this.getView()?.getModel("selectionScreen")?.getProperty("/ShipName");
+        const CountryKey = this.getView()?.getModel("selectionScreen")?.getProperty("/CountryKey");
+        let filters = [];
+
+        if (ShipName !== "") {
+            filters.push(new Filter("ShipName", FilterOperator.Contains, ShipName))
+        }
+
+        if (CountryKey !== "") {
+            filters.push(new Filter("Country", FilterOperator.EQ, CountryKey))
+        }
+
+        const oList = this.getView()?.byId("invoicesList");
+        const oBinding = oList?.getBinding("items") as ListBinding;
+        oBinding?.filter(filters)
+    }
+
+    public onClearFilter(): void{
+       const oModelSelScreen = this.getView()?.getModel("selectionScreen") as JSONModel;
+       oModelSelScreen?.setProperty("/ShipName", "");
+       oModelSelScreen?.setProperty("/CountryKey", "");
+       
+       const oList = this.getView()?.byId("invoicesList");
+       const oBinding = oList?.getBinding("items") as ListBinding;
+       oBinding?.filter([]);
+    }
+
 }
